9d6112db34119be373a0592ef53784da1df13995
yuriybobyk <55201873+yuriybobyk@users.noreply.github.com>
2019-11-13 13:17:23 +0200

    Lab_3: update README.md

--- lab_3/main/views.py
+++ lab_3/main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+import platform
+from datetime import datetime
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+
+def health(request):
+    response = {'date': datetime.today(),
+                'current_page': request.path,
+                'server_info': {
+                    'system': platform.system(),
+                    'version': platform.version(),
+                    'release': platform.release(),
+                    'network name': platform.node(),
+                    'machine': platform.machine(),
+                    'processor': platform.processor()
+                },
+                'client_info': request.META['HTTP_USER_AGENT']}
+    return JsonResponse(response)
